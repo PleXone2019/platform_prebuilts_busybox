4fd45348a7a50d001ed0a9a90286528a07d90a3d
binkybear <nethunteros@users.noreply.github.com>
2016-10-27 21:38:05 -0500

    Copy to /system/xbin instead of /system/bin
    Rename to busybox_nh

--- Android.mk
+++ Android.mk
@@ -17,10 +17,11 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := busybox
+LOCAL_MODULE := busybox_nh
 LOCAL_MODULE_CLASS := EXECUTABLES
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_TARGET_ARCH := arm
 LOCAL_MODULE_TARGET_ARCH_WARN := arm
+LOCAL_MODULE_PATH := $(TARGET_OUT)/xbin
 LOCAL_SRC_FILES_arm := arm/busybox_nh
 include $(BUILD_PREBUILT)
